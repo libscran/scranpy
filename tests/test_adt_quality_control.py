d7f2539d2aa90ca814bbb1488db65a502ae8f53e
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-12-18 20:37:30 +1100

    Begin adding the SE-compatible wrappers around each function.

--- tests/test_adt_quality_control.py
+++ tests/test_adt_quality_control.py
@@ -91,3 +91,27 @@ def test_suggest_adt_qc_thresholds_blocked():
         block=block[last]
     )
     assert (observed[last] == last_observed).all()
+
+
+def test_quick_adt_qc_se():
+    import summarizedexperiment
+    y = (numpy.random.rand(100, 1000) * 5).astype(numpy.uint32)
+    se = summarizedexperiment.SummarizedExperiment(
+        { "counts": y },
+        column_data = biocframe.BiocFrame({ "foo": numpy.random.rand(1000) })
+    )
+
+    out = scranpy.quick_adt_qc_se(se, subsets={ "igg": [1,2,3] })
+    assert "sum" in out.get_column_data().get_column_names()
+    assert "detected" in out.get_column_data().get_column_names()
+    assert "subset_sum_igg" in out.get_column_data().get_column_names()
+    assert out.get_metadata()["qc"]["thresholds"] is not None
+    assert "foo" in out.get_column_data().get_column_names() # existing columns are still preserved.
+
+    # Works if we slap in a prefix.   
+    out = scranpy.quick_adt_qc_se(se, subsets={ "igg": [1,2,3] }, output_prefix="FOO_")
+    assert "FOO_sum" in out.get_column_data().get_column_names()
+
+    # Works if we skip the metadata.
+    out = scranpy.quick_adt_qc_se(se, subsets=[], meta_name=None)
+    assert "qc" not in out.get_metadata()
