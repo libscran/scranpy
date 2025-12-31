0ac0b8b883c4d2da9be126fd3cc0dc406379f12e
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2025-12-31 16:27:29 +1100

    Support gene names in the sets= for aggregate_across_genes().

--- src/scranpy/aggregate_across_genes.py
+++ src/scranpy/aggregate_across_genes.py
@@ -10,6 +10,7 @@ from . import lib_scranpy as lib
 def aggregate_across_genes(
     x: Any,
     sets: Sequence,
+    row_names: Optional[Sequence] = None,
     average: bool = False,
     num_threads: int = 1
 ) -> biocutils.NamedList:
@@ -22,9 +23,16 @@ def aggregate_across_genes(
             Values are expected to be log-expression values.
 
         sets:
-            Sequence of integer arrays containing the row indices of genes in each set.
-            Alternatively, each entry may be a tuple of length 2, containing an integer vector (row indices) and a numeric vector (weights).
+            Sequence of sequences containing strings or integers.
+            Each sequence corresponds to a gene set and contains the row indices or names of the genes in that set.
+            If any strings are present, ``row_names`` should be supplied.
+
+            Alternatively, each entry may be a tuple of length 2, containing a sequence of strings/integers (row names/indices) and a numeric array (weights).
             If this is a :py:class:`~biocutils.NamedList.NamedList`, the names will be preserved in the output.
+
+        row_names:
+            Sequence of strings of length equal to the number of rows of ``x``, containing the name of each gene.
+            If ``None``, rows are assumed to be unnamed.
 
         average:
             Whether to compute the average rather than the sum.
@@ -41,14 +49,15 @@ def aggregate_across_genes(
         The ``aggregate_across_genes`` function in the `scran_aggregate <https://libscran.github.io/scran_aggregate>`_ C++ library, which implements the aggregation.
     """ 
     new_sets = [] 
+    mapping = {}
     for s in sets:
         if isinstance(s, tuple):
             new_sets.append((
-                numpy.array(s[0], copy=None, order="A", dtype=numpy.uint32),
-                numpy.array(s[1], copy=None, order="A", dtype=numpy.float64)
+                _check_for_strings(s[0], mapping, row_names),
+                numpy.array(s[1], copy=None, order="A", dtype=numpy.float64),
             ))
         else:
-            new_sets.append(numpy.array(s, copy=None, order="A", dtype=numpy.uint32))
+            new_sets.append(_check_for_strings(s, mapping, row_names))
 
     mat = mattress.initialize(x)
     output = lib.aggregate_across_genes(
@@ -62,3 +71,34 @@ def aggregate_across_genes(
     if isinstance(sets, biocutils.NamedList):
         names = sets.get_names()
     return biocutils.NamedList(output, names)
+
+
+def _check_for_strings(y: Sequence, mapping: dict, row_names: Optional[Sequence]):
+    has_str = False
+    for x in y:
+        if isinstance(x, str):
+            has_str = True
+            break
+
+    if not has_str:
+        return numpy.array(y, copy=None, order="A", dtype=numpy.uint32)
+
+    if "realized" not in mapping:
+        if row_names is None:
+            raise ValueError("no 'row_names' supplied for mapping gene sets with names")
+        found = {}
+        for i, s in enumerate(row_names):
+            if s not in found:
+                found[s] = i
+        mapping["realized"] = found
+    else: 
+        found = mapping["realized"]
+
+    output = numpy.ndarray(len(y), dtype=numpy.uint32)
+    for i, x in enumerate(y):
+        if isinstance(x, str):
+            output[i] = found[x]
+        else:
+            output[i] = x
+
+    return output
--- tests/test_aggregate_across_genes.py
+++ tests/test_aggregate_across_genes.py
@@ -21,7 +21,7 @@ def test_aggregate_across_genes_unweighted():
     for i, ss in enumerate(sets):
         assert numpy.allclose(agg[i], x[ss,:].mean(axis=0))
 
-    # Works with names.
+    # Works with set names.
     names = ["foo", "bar", "whee"]
     agg = scranpy.aggregate_across_genes(x, biocutils.NamedList(sets, names))
     assert agg.get_names().as_list() == names
@@ -55,3 +55,25 @@ def test_aggregate_across_genes_weighted():
 
     with pytest.raises(Exception, match = "equal length"):
         scranpy.aggregate_across_genes(x, [([0], [1,2,3])])
+
+
+def test_aggregate_across_genes_names():
+    x = numpy.random.rand(1000, 100)
+    sets = [
+        (numpy.random.rand(20) * x.shape[0]).astype(numpy.int32),
+        (numpy.random.rand(10) * x.shape[0]).astype(numpy.int32),
+        (numpy.random.rand(500) * x.shape[0]).astype(numpy.int32)
+    ]
+    agg = scranpy.aggregate_across_genes(x, sets)
+
+    # Works with gene names.
+    names = ["GENE_" + str(i) for i in range(x.shape[0])]
+    named_sets = [biocutils.subset(names, i) for i in sets]
+    named_agg = scranpy.aggregate_across_genes(x, named_sets, row_names=names)
+
+    assert len(agg) == len(named_agg)
+    for i in range(len(sets)):
+        assert (agg[i] == named_agg[i]).all()
+
+    with pytest.raises(Exception, match="no 'row_names' supplied"):
+        scranpy.aggregate_across_genes(x, named_sets)
